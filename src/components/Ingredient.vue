0e0ebab072b6ab138f4cbdf7e6a6ee6d77880d97
Grape Machine <jmw686@nyu.edu>
2018-04-29 19:23:43 -0500

    set up components

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -7,11 +7,21 @@
 export default {
   data () {
     return {
-        
+        name: "",
+        size: 0,
+        price: 0
     }
   },
   methods: {
-   
+      setName(newName) {
+          this.name = newName; 
+      },
+      setSize(newSize) {
+          this.size = newSize; 
+      },
+      setPrice(newPrice) {
+          this.price = newPrice; 
+      }
   }
 
 }
