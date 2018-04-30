0aac1ac73b6aa8b8f433eedae870e7d05a124366
Grape Machine <jmw686@nyu.edu>
2018-04-29 19:25:00 -0500

    update app & add some create comp

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,43 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <CreateIngredient v-on:create-ingredient="createIngredient"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import CreateIngredient from './components/CreateIngredient'
+import Ingredient from './components/Ingredient'
+import List from './components/List'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CreateIngredient,
+    Ingredient,
+    List
+  },
+  data() {
+    return {
+      recipe: [{
+
+      }],
+      buylist: [{
+
+      }]
+    }
+  },
+  methods: {
+    createIngredient(newIngredient) {
+      if (newIngredient.buylist) {
+        this.buylist.push(newIngredient);
+      }
+      else {
+        this.recipe.push(newIngredient);
+      }
+    }
   }
 }
 </script>
---
+++ src/components/CreateIngredient.vue
@@ -0,0 +1,68 @@
+<template lang="html">
+  <div class="create-ingredient">
+    <div>
+        <label>Name</label>
+        <input v-model="name" type='text'>
+        <br/>
+        <label>Size (in oz)</label>
+        <input v-model="size" type='text'>
+        <br/>
+        <label>Price</label>
+        <input v-model="price" type='text'>
+        <br/>
+        <input v-model="buylist" type='checkbox'> Put this on BuyList
+        <br/>
+        <button v-on:click="sendForm" class="">
+          Add To Recipe
+        </button>
+        
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+        name: "",
+        size: 0,
+        price: 0,
+        buylist: false
+    }
+  },
+  methods: {
+    sendForm() {
+      if (this.name.length > 0 && this.size > 0) {
+        const name = this.name;
+        const size = this.size;
+        const price = this.price;
+        const buylist = this.buylist;
+        if (buylist) {
+            this.$emit('create-ingredient', {
+              name: this.name,
+              size: this.size,
+              price: this.price,
+              buylist: true
+            });
+        }
+        else {
+            this.$emit('create-ingredient', {
+              name: this.name,
+              size: this.size,
+              price: this.price
+            });
+        }
+        this.name = '';
+        this.size = 0;
+        this.price = 0;
+        this.buylist = false;
+      } 
+    } 
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
+
