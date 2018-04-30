0b2abb9f45620003db7cc3318e7404aed19024cc
Grape Machine <jmw686@nyu.edu>
2018-04-30 17:30:50 -0500

    .

---
+++ src/components/Buylist.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+    <div class="list" v-if='buylist.length'>
+        <ul>
+            <li v-for="ingredient in buylist">
+            <Ingredient :ingredient.sync="ingredient" />
+            </li>
+        </ul>
+        <div>
+            <button @click='calc(buylist)'>calc {{this.sum}}</button>
+            <div v-if='this.sum > 0'> {{this.sum}}</div>
+        </div>
+    </div>
+</template>
+<script>
+import Ingredient from './Ingredient';
+export default {
+  props: ['buylist'],
+  components: {
+    Ingredient
+  },
+  data () {
+    return {
+        sum: null
+    }
+  },
+  methods: {
+      calc(buylist) {
+          let total = 0;
+          for (let i = 0; i < buylist.length; i++) {
+            total += parseInt(buylist[i].price);
+          }
+          this.sum = total;
+          console.log(total + ' aaa00');
+      }
+  },
+  computed: {
+      total(buylist) {
+          let total = 0;
+          for (let i = 0; i < buylist.length; i++) {
+            total += parseInt(buylist[i].price);
+          }
+          this.sum = total;          
+      }
+  }
+}
+</script>
+<style lang="css" scoped>
+</style>
